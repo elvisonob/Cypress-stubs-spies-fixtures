187f156d9b033425abf64c749f907d39a6136127
elvisonob <elvisonob@yahoo.com>
2023-10-25 05:00:11 +0100

    adding Stubs

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -2,7 +2,14 @@
 
 describe('share location', () => {
   it('should fetch the user location', () => {
-    cy.visit('/');
+    cy.visit('/').then((win) => {
+      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as(
+        'getUserPosition'
+      );
+    });
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('@getUserPosition').should('have.been.called');
+    cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
+    cy.get('[data-cy="actions"]').should('contain', 'Location fetched');
   });
 });
